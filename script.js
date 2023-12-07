19e2eb3dd2dbfc2d84a9c41ee5bf682c76572d99
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-12-07 12:13:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const btn = document.querySelector(".btn");
+const txt = document.querySelector(".text");
+
+btn.addEventListener("click", () => {
+  txt.classList.toggle("text");
+});
