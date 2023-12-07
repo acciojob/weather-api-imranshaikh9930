5fb2bf38370d5ea3d42a14d3c044c2a46934a745
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-12-07 12:36:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-//your JS code here. If required.
-const btn = document.querySelector(".btn");
-const txt = document.querySelector(".text");
+import "./styles.css";
 
-btn.addEventListener("click", () => {
-  txt.classList.toggle("text");
-});
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", getCurrentWeather);
+function getCurrentWeather() {
+  // console.log("click");
+  // Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
+  const apiKey = "7c29a3a0b8862faeb96ca3b1be764114";
+  const city = "London";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  // Make the API request
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      // Extract the weather description from the API response
+
+      // console.log(data);
+      const weatherDescription = data.weather[0].description;
+
+      console.log(weatherDescription);
+
+      // Update the content of the weatherData div
+      const weatherDataDiv = document.getElementById("weatherData");
+      weatherDataDiv.textContent = `Current weather in ${city}: ${weatherDescription}`;
+    })
+    .catch((error) => {
+      console.error("Error fetching weather data:", error);
+      // const weatherDataDiv = document.getElementById("weatherData");
+      // weatherDataDiv.textContent =
+      //   "Error fetching weather data. Please try again later.";
+    });
+}
+
+// getCurrentWeather();
